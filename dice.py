5f45d46e877d0e60d5e3e71a22b91032a890a205
Vincenzo <35369226+risolutore@users.noreply.github.com>
2021-11-13 08:10:57 +0100

    Update dice.py

--- dice.py
+++ dice.py
@@ -6,28 +6,47 @@ class Dice():
         # Numero di facce (number of faces)
         self.diceFaces = facesNumber
         #risultato del lancio (launch result)
-        self.result = 1
+        self.result = 0
 
-    def getLastRoll(self):
+    def lastRoll(self):
+        # Ritorna ultimo risultato (return last result)
         return self.result
 
-    def roll(self):
+    def roll(self) -> int:
         rnd.seed()
+        lancio = 0
         launches = []
 
         # generate random rolls list
         for i in range(1, 5000):
             launches.append(rnd.randint(1, self.diceFaces))
 
-        self.result = launches[rnd.randint(3000, 4999)]
+        # Get random result from list of launces
+        lancio = launches[rnd.randint(3000, 4999)]
+        self.result = lancio
+        launches = []
+        # return the result as  dice roll
+        return lancio
 
-        return self.result
+    def multiRoll(self, launces) -> int:
+        self.result = 0
+        lanci = 0
 
-
+        for i in range(0, launces):
+            lanci += self.roll()
+            self.result = lanci
+        
+        return lanci
 
 """
 # Example use:
+
+# Single Roll
 d20 = Dice(20)
 print(d20.roll())
 
+# Multi-Roll
+thrwos = 3
+d10 = Dice(10)
+print(d10.multiRoll(throws))
 """
