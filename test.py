1a04963d436c15f6e5ee1e5a14041ed283f5d3f8
Vincenzo <35369226+risolutore@users.noreply.github.com>
2023-09-23 20:17:37 +0200

    Update test.py
    Added some useful comments to the code

--- test.py
+++ test.py
@@ -1,16 +1,22 @@
 from diceparser import Tokenizer, ExpressionParser
 from dice import Dice
 
-
+# Dice initial expression
 expr = '5d10+2d6+6'
 
+# Need to set the exression like a list
+# if none add at the biginnign of the expression string a '['
 if expr[0] != '[':
     expr = f"[{expr}"
 
+# i none add at the end of the expression string a ']'
 if expr[len(expr) - 1] != ']':
     expr = f"{expr}]"
 
+# now we have:
+# expr = '[5d10+2d6+6]'
 
+# now it's time to start parsing
 try:
     lexer = Tokenizer(expr)
     parser = ExpressionParser(lexer)
